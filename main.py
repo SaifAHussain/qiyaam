ed7a7e2f4966d2ff382f091c08c589c6c8ceffc3
Waddy <waddyhussain@yahoo.co.uk>
2024-08-25 19:00:19 +0100

    resolve conflicts

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from datetime import datetime, timedelta, UTC
+import datetime as dt
 import pytz
 from typing import Optional
 from fastapi import FastAPI
@@ -9,15 +9,30 @@ uk = pytz.timezone('Europe/London')
 with open("prayer_times.json", "r") as f:
     prayer_times = json.load(f)
 
-def adjust_for_dst(time):
-    pass
+def adjust_for_DST(utc_time):
+    if type(utc_time) == str:
+        dt_object = dt.datetime.strptime(utc_time,"%H:%M")
+        DST_time = dt_object.astimezone(uk)
+        return dt.datetime.strftime(DST_time, "%H:%M")
+    if type(utc_time) == dict:
+        for key, value in utc_time.items():
+            utc_time[key] = adjust_for_DST(value)
 
+        return utc_time
+    if type(utc_time) == list:
+        for i, item in enumerate(utc_time):
+            utc_time[i] = adjust_for_DST(item)
+
+        return utc_time
+
+
+# print(adjust_for_DST("10:24"))
 app = FastAPI()
 
 # Today
 @app.get("/{location}/today")
 def get_today(location: str):
-    today = datetime.now(UTC).astimezone(uk)
+    today = dt.datetime.now(dt.UTC).astimezone(uk)
     return {
         "body": prayer_times[location][today.month-1][today.day-1]
     }
@@ -25,7 +40,7 @@ def get_today(location: str):
 # Tomorrow
 @app.get("/{location}/tomorrow")
 def get_tomorrow(location: str):
-    tomorrow = datetime.now(UTC).astimezone(uk) + timedelta(days=1)
+    tomorrow = dt.datetime.now(dt.UTC).astimezone(uk) + dt.timedelta(days=1)
     return {
         "body": prayer_times[location][tomorrow.month-1][tomorrow.day-1]
     }
@@ -33,7 +48,7 @@ def get_tomorrow(location: str):
 # Yesterday
 @app.get("/{location}/yesterday")
 def get_yesterday(location: str):
-    yesterday = datetime.now(UTC).astimezone(uk) - timedelta(days=1)
+    yesterday = dt.datetime.now(dt.UTC).astimezone(uk) - dt.timedelta(days=1)
     return {
         "body": prayer_times[location][yesterday.month-1][yesterday.day-1]
     }
@@ -67,7 +82,7 @@ def get_day(location: str, month: int, day: int):
 def get_time(location: str, month: int, day: int, time: str):
 
     utc_datetime_str = prayer_times[location.lower()][month-1][day-1][time.lower()]
-    utc_datetime_object = datetime.strptime(utc_datetime_str,"%H:%M")
+    utc_datetime_object = dt.datetime.strptime(utc_datetime_str,"%H:%M")
     london_datetime_object = utc_datetime_object.astimezone(uk)
 
     return {
